672734a127af1f088755797337325f2e9125aed4
Thomas-Glaud <thomascoder223@gmail.com>
2026-05-10 19:12:31 +0100

    Filled in Readme, created js and css files, made the pixel selection and cursor work

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-**_Color codes:_**
+# Important notes:
+
+- When changing matrix size, remember to change it both in `main.py` and `script.js`
+
+## SSE messages format
+
+Library with:
+**"t"** = message type (m for move and p for pixel)
+**"x" and "y"** = new positions for cursor / positions of new pixel
+**"c"** = color index
+
+## Color indexes:
+
+"r": "red"
+"g": "green"
+"b": "blue"
+"y": "yellow"
+"c": "cyan"
+"m": "magenta"
+"w": "white"
+"k": "black"
---
+++ works_like_controller/esp32 wifi test/script.js
@@ -0,0 +1,58 @@
+console.log("Script loaded");
+let color_codes = {
+            "r": "red",
+            "g": "green",
+            "b": "blue",
+            "y": "yellow",
+            "c": "cyan",
+            "m": "magenta",
+            "w": "white",
+            "k": "black"
+        }
+
+const canvas = document.getElementById('pixelCanvas');
+const cursor = document.getElementById('circle_cursor');
+const ctx = canvas.getContext('2d');
+        
+const MATRIX_SIZE = { width: 100, height: 50 };
+const PIXEL_SIZE = { width: 800 / MATRIX_SIZE.width, height: 400 / MATRIX_SIZE.height };
+
+const evtSource = new EventSource('/events');
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const rect = canvas.getBoundingClientRect();
+  
+
+function drawPixelOnCanvas(x, y, color) {
+    ctx.fillStyle = color_codes[color] || "white"; // Default to white if color code is unknown
+    ctx.fillRect(x * PIXEL_SIZE.width, y * PIXEL_SIZE.height, PIXEL_SIZE.width, PIXEL_SIZE.height);
+}
+
+function moveCursorHighlight(x, y) {
+    const cursor_x = x * PIXEL_SIZE.width + PIXEL_SIZE.height / 2 + rect.left;
+    const cursor_y = y * PIXEL_SIZE.height + PIXEL_SIZE.height / 2 + rect.top;
+            
+    cursor.style.transform = `translate(${cursor_x}px, ${cursor_y}px)`;
+}
+
+
+evtSource.onmessage = (event) => { //wait for data from the server
+
+    let data = JSON.parse(event.data); 
+
+    if (data.t === "p") {
+        drawPixelOnCanvas(data.x, data.y, data.c); // Draw the pixel on the canvas
+    } 
+    else if (data.t === "c") {
+        moveCursorHighlight(data.x, data.y); // Move the cursor highlight
+    }
+};
+
+drawPixelOnCanvas(50, 25, "g"); // Test pixel
+drawPixelOnCanvas(50, 30, "y"); // Test pixel
+drawPixelOnCanvas(30, 25, "b"); // Test pixel
+drawPixelOnCanvas(10, 10, "c"); // Test pixel
+drawPixelOnCanvas(99, 49, "m"); // Test pixel
+moveCursorHighlight(20, 25); // Test cursor highlight
